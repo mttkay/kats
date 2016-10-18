172f2fb39d77597c8be9c9f94186538df0916bee
Matthias Kaeppler <m.kaeppler@gmail.com>
2016-10-18 21:45:10 +0200

    Move nullableMonoid to predef

--- src/main/kotlin/com/github/mttkay/kats/ext/nullable/NullableExt.kt
+++ src/main/kotlin/com/github/mttkay/kats/ext/nullable/NullableExt.kt
@@ -1,11 +1,5 @@
 package com.github.mttkay.kats.ext.nullable
 
-import com.github.mttkay.kats.Monoid
-import com.github.mttkay.kats.Semigroup
-
-fun <T> nullableMonoid(sg: Semigroup<T>): Monoid<T?> = Monoid.of(null as T?) { a, b ->
-  sg.maybeCombine(a, b)
-}
 
 inline fun <A, B> A?.fold(ifEmpty: B, f: (A) -> B): B =
     when {
--- src/main/kotlin/com/github/mttkay/kats/predef/predef.kt
+++ src/main/kotlin/com/github/mttkay/kats/predef/predef.kt
@@ -1,14 +1,20 @@
 package com.github.mttkay.kats.predef
 
 import com.github.mttkay.kats.Monoid
-import com.github.mttkay.kats.ext.nullable.nullableMonoid
+import com.github.mttkay.kats.Semigroup
 
+/*
+Monoids
+ */
 val AddIntMonoid: Monoid<Int> by lazy { Monoid.of(0) { a, b -> a + b } }
 val AddLongMonoid: Monoid<Long> by lazy { Monoid.of(0L) { a, b -> a + b } }
 val AddFloatMonoid: Monoid<Float> by lazy { Monoid.of(0F) { a, b -> a + b } }
 val AddDoubleMonoid: Monoid<Double> by lazy { Monoid.of(0.0) { a, b -> a + b } }
 val AddStringMonoid: Monoid<String> by lazy { Monoid.of("") { a, b -> a + b } }
 
+private fun <T> nullableMonoid(sg: Semigroup<T>): Monoid<T?> = Monoid.of(null as T?) { a, b ->
+  sg.maybeCombine(a, b)
+}
 val AddNullableIntMonoid: Monoid<Int?> by lazy { nullableMonoid(AddIntMonoid) }
 val AddNullableLongMonoid: Monoid<Long?> by lazy { nullableMonoid(AddLongMonoid) }
 val AddNullableFloatMonoid: Monoid<Float?> by lazy { nullableMonoid(AddFloatMonoid) }
